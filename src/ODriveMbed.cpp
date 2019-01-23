67772cd19582b93d7aacb99b194142b61bcdcf7c
stoneil <seanogon@gmail.com>
2019-01-22 22:16:24 -0500

    fixed the copy-pasta induced typo to try and make this function more robust

--- src/ODriveMbed.cpp
+++ src/ODriveMbed.cpp
@@ -112,14 +112,14 @@ int ODriveMbed::readControlMode(int axis){
 
 int ODriveMbed::readState(int axis){
     int timeout_ctr = 5;
-    float current = 433223465.0f;
+    int state = -1;
         do {
             wait_ms(_timeoutTime);
             //serial_ << "r axis" << axis << ".current_state\n";
             serial_.printf("r axis%d.current_state\n",axis);
-            current = readInt();
+            state = readInt();
         } while (--timeout_ctr > 0);
-    return current;
+    return state;
 }
 
 string ODriveMbed::readString() {
